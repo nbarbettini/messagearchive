be09b23a6662b2062ee7fa2a0a82075ccc16f340
Nate Barbettini <nathanaelb@gmail.com>
2015-09-20 13:22:03 -0700

    Better search and more demo functionality

--- MessageArchive/Controllers/MessageController.cs
+++ MessageArchive/Controllers/MessageController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using MessageArchive.Models;
@@ -10,24 +11,32 @@ namespace MessageArchive.Controllers
 {
     public class MessageController : ApiController
     {
-        public async Task<IEnumerable<Message>> Get(string q)
+        public async Task<IEnumerable<Message>> Get(string q, int from, int size)
         {
             var url = ConfigurationManager.AppSettings["ES_URL"];
             var setting = new ConnectionSettings(new Uri(url));
             var client = new ElasticClient(setting);
 
+            // Parse the search query
+            string[] terms = q.Split(' ');
+            var personTerm = terms.SingleOrDefault(x => x.StartsWith("person:"));
+            if (!string.IsNullOrEmpty(personTerm))
+            {
+                terms = terms.Except(new string[] { personTerm }).ToArray();
+                personTerm = personTerm.Replace("person:", string.Empty);
+            }
+            var textTerms = string.Join(" ", terms);
+
             var searchResults = await client.SearchAsync<Message>(s => s
                 .AllIndices()
                 .AllTypes()
-                .Size(50)
+                .Size(size)
+                .From(from)
                 .SortAscending(x => x.Date)
-                .Query(query =>
-                    query.MultiMatch(mm => 
-                        mm.OnFields(
-                            f => f.From, 
-                            f => f.To, 
-                            f => f.Text)
-                        .Query(q))));
+                .Query(qry =>
+                    (qry.Term("from", personTerm) ||
+                    qry.Term("to", personTerm)) &&
+                    qry.MatchPhrase(mp => mp.OnField(f => f.Text).Query(textTerms))));
 
             return searchResults.Documents;
         }
---
+++ MessageArchive/demo.js
@@ -0,0 +1,52 @@
+﻿var archiveApp = angular.module('archiveApp', ['infinite-scroll']);
+
+archiveApp.controller('DemoController', function ($scope, Archive) {
+    $scope.archive = null;
+    $scope.query = '';
+
+    $scope.$watch("query", function (newValue, oldValue) {
+        $scope.archive = new Archive(newValue);
+        $scope.archive.more();
+    });
+});
+
+// constructor function to encapsulate HTTP and pagination logic
+archiveApp.factory('Archive', function ($http) {
+    var Archive = function (query) {
+        this.items = [];
+        this.atEnd = false;
+        this.busy = false;
+        this.query = query;
+        this.offset = 0;
+        this.size = 10;
+        this.error = false;
+
+        if (query.length === 0)
+            this.atEnd = true;
+    };
+
+    Archive.prototype.more = function () {
+        if (this.atEnd) return;
+        if (this.busy) return;
+        this.busy = true;
+
+        var url = "http://localhost:55039//api/message?q=" + this.query + "&from=" + this.offset + "&size=" + this.size;
+        $http.get(url).then(
+            function (response) {
+                for (var i = 0; i < response.data.length; i++) {
+                    var newItem = response.data[i];
+                    newItem.Date = moment(newItem.Date); // wrap in Moment object
+                    this.items.push(newItem);
+                }
+                this.offset += response.data.length - 1;
+                this.atEnd = response.data.length < (this.size - 1);
+                this.busy = false;
+            }.bind(this),
+            function (errorResponse) {
+                debugger;
+                this.error = true;
+            }.bind(this));
+    };
+
+    return Archive;
+});
